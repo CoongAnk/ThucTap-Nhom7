3f1e4980309383144b47b44686a65928df9d8c93
CoongAnk <conganh8864@gmail.com>
2026-02-03 17:52:50 +0700

    Fix Styles/CSS

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,12 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 // GLOBAL CSS
-import './styles/reset.css'
-import './styles/header.css'
-import './styles/footer.css'
-import './styles/style.css'
 import './index.css'
-import './styles/Login.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+import '../styles/Home.css';
+
 const Home = () => {
   return (
     <main className="container">
