b9acdc5bf291817b34eebd8a33033226f7ef8f5f
xuanchinh <131693893+xuchinh@users.noreply.github.com>
2026-03-05 17:56:48 +0700

    xóa

--- Backend/routes/aiTutor.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { chatWithTutor } from "../controllers/aiTutor.controller.js";
-
-const router = express.Router();
-router.post("/tutor", chatWithTutor);
-
-export default router;
